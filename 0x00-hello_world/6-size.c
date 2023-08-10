ddf4d84e7efbcf24cce1f65259c386a0c812c409
Isaac <mugagga10@gmail.com>
2023-08-10 14:19:11 -0700

    correction 3 to task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %zu bytes\n", size_t(char));
-	printf("Size of an int: %zu bytes\n", size_t(int));
-	printf("Size of a  long: %zu bytes\n", size_t(long));
-	printf("Size of a long long: %zu bytes\n", size_t(long long));
-	printf("Size of a float: %zu bytes\n", size_t(float));
+	printf("Size of a char: %zu bytes\n", sizeof(char));
+	printf("Size of an int: %zu bytes\n", sizeof(int));
+	printf("Size of a  long: %zu bytes\n", sizeof(long));
+	printf("Size of a long long: %zu bytes\n", sizeof(long long));
+	printf("Size of a float: %zu bytes\n", sizeof(float));
 	return (0);
 }
