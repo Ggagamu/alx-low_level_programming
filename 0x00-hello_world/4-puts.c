1fff9368a60a4228d0199a744066f7d66dbecf61
Isaac <mugagga10@gmail.com>
2023-08-10 12:02:08 -0700

    5th correction to task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/*This is the main function of the program. */
+
+/**
+ *  This is the main function of the program. 
+ */
 int  main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
