42fa37203ca23a2c53b6b926014aaaf1c8dc52ee
Isaac <mugagga10@gmail.com>
2023-08-10 12:38:21 -0700

    10th correction of task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main -  Entry point of the program.
- * @return - Returns 0 to show successful execution.
+ * @return: Returns 0 to show successful execution.
  */
 int  main(void)
 {
