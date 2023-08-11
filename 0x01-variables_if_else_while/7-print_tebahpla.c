4d70aedafc536ad4e46160a656809fe2b3ba44b4
Isaac <mugagga10@gmail.com>
2023-08-11 16:51:35 -0700

    check 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in reverse.
+ * Return: 0 to show success.
+ */
+int main(void)
+{
+	char n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	for (n = 'z'; n >= 'a'; n++)
+	{
+		putchar (n);
+	}
+	putchar('\n');
+	return (0);
+}
