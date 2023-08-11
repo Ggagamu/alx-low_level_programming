cebd6956b5202f529b5de13d10e6d6c8d55af075
Isaac <mugagga10@gmail.com>
2023-08-11 16:31:29 -0700

    correction task 6

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,12 +2,12 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Prints the alphabet.
+ * main - Prints single digit numbers.
  * Return: 0 to show success.
  */
 int main(void)
-{
-	char n;
+{	
+	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
