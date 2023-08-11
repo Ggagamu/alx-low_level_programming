ba6078cb1b7148f04d144f94fdf9c10c2dd30199
Isaac <mugagga10@gmail.com>
2023-08-11 16:28:21 -0700

    check task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Prints the alphabet.
+ * Return: 0 to show success.
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	for (n = '0'; n < '10'; n++)
+	{
+		putchar(n + '0');
+	}	
+	putchar('\n');
+	return (0);
