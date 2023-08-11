3fe290be163177d3ac98c57adc2ab2f05977e919
Isaac <mugagga10@gmail.com>
2023-08-11 12:11:51 -0700

    first check for correctness

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - A program that shows whether the number stored in the variable n is
+ * positive or negative with printf function.
+ * Return: 0 to show success.
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n>0)
+	{
+		printf("n is positive\n");
+	}
+	else
+	{
+		printf("n is positive\n");
+	}
+	return (0);
+}
