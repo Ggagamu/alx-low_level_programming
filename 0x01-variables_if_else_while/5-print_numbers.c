d30642ba430a48bed036db64b4695d0ec174a72f
Isaac <mugagga10@gmail.com>
2023-08-11 15:10:32 -0700

    check task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Prints the alphabet.
+ * Return: 0 to show success.
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+        for (n = '0'; n <= '9'; n++)
+	{
+		printf("%d\n", n);
+	}
+        return (0);
+}
