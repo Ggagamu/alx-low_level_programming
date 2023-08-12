b69cf977226dc08b9c1a12f3334c1d18cfc35f95
Isaac <mugagga10@gmail.com>
2023-08-11 17:47:34 -0700

    check 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Prints the alphabet.
+ * Return: 0 to show success.
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	for (n = 0; n <= 9; n++)
+	{
+		if (n == 9)
+			putchar(n + '0');
+		else
+		{
+			putchar(n + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
