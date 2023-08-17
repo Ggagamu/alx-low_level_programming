81c7dbcde9c33a638a1afecd827c916c255a1dd0
Isaac <mugagga10@gmail.com>
2023-08-17 16:13:10 -0700

    check task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *print_numbers - Prints numbers 0 t0 9.
+ */
+void print_numbers(void);
+{
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		_putchar(number);
+	}
+	_putchar('\n');
+}
