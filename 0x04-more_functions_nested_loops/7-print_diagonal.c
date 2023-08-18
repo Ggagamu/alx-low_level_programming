85474cb9875e29c04ac7aa9bbf52a28e14aae280
Isaac <mugagga10@gmail.com>
2023-08-17 17:49:08 -0700

    chek 3

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,11 +5,15 @@
  */
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar('\');
+		for (j = 0; j < n; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
