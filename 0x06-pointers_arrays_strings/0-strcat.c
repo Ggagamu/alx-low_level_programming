cc653618c7226616409e9629185f5e79c4a07e7c
Isaac <mugagga10@gmail.com>
2023-08-23 08:45:19 -0700

    first check

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * *_strcat - concatenates two strings
+ * @*dest: string to be concatenated
+ * @*src: string to be concatenated
+ * Return - *dest 
+ */
+char *_strcat(char *dest, char *src)
+{
+	*_strcat(dest, src);
+	return (*dest);
+}
