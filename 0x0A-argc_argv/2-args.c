16354928468b94d881ca6fcff8793b8e62bd225e
Isaac <mugagga10@gmail.com>
2023-09-02 07:51:37 -0700

    check 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints all arguments it receives
+ * @argc: arguments counter
+ * @argv: arguments volume
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0])
+	}
+
+	return (0);
+}
