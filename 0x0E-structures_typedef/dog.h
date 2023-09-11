8063de8be0759fa6c05e87497d6ef447b03307ea
Isaac <mugagga10@gmail.com>
2023-09-11 07:54:42 -0700

    check 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,5 +12,16 @@ struct Dog
 	float *age;
 	char *owner;
 };
+/**
+ * dog_k - typedef for struct Dog
+ */
+typedef struct Dog dog_k;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif /*DOG_H*/
